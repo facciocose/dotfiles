010cfc042a5c21979f7ba4307c39ea46d60763e1
Luca <ldm.marra@gmail.com>
2022-11-03 00:47:41 +0100

    updated vim config

--- nvim/.config/nvim/lua/config.lua
+++ nvim/.config/nvim/lua/config.lua
@@ -1,17 +1,16 @@
 local lsp = require "lspconfig"
--- local coq = require "coq"
+local opts = { noremap=true, silent=true }
 
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 
--- local on_attach = function(client, bufnr)
---   vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
--- end
--- 
--- local coq_lsp_config = coq.lsp_ensure_capabilities{
---   on_attach = on_attach
--- }
+local on_attach = function(client, bufnr)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+end
 
--- lsp.tsserver.setup(coq_lsp_config)
--- lsp.pyright.setup(coq_lsp_config)
+lsp.tsserver.setup{
+  on_attach = on_attach
+}
+lsp.pyright.setup{
+  on_attach = on_attach
+}
 
-lsp.tsserver.setup{}
-lsp.pyright.setup{}
